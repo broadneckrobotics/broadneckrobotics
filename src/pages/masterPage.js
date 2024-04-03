7a890d86c9306dea744672c71fa08d9f3e973261
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-04-03 23:28:37 +0000

    Initial commit

---
+++ src/pages/masterPage.js
@@ -0,0 +1,10 @@
+// For full API documentation, including code examples, visit http://wix.to/94BuAAs
+
+$w.onReady(function () {
+	//TODO: write your page related code here...
+
+});
+
+export function footer1_viewportEnter(event, $w) {
+	//Add your code for this event here: 
+}
